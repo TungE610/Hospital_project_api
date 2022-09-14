c6a5a7f2bdf9262d7881ab9d5e2d52c4808e46fa
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2022-09-14 19:54:26 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 const express = require('express')
-require('dotenv').config()
 const bodyParser = require('body-parser')
 const app = express()
-const cookieParser = require("cookie-parser");
 const cors = require('cors')
 const pool = require('../src/app/config/db.config')
+require('dotenv').config()
 const bcrypt = require('bcrypt')
-console.log(process.env.DB_PORT)
-app.listen(process.env.DB_PORT || 5000, () => {
-	console.log(`The server is running in ${process.env.DB_PORT || 5000}`)
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`The server is running in ${process.env.DB_PORT}`)
 })
 const corsOptions ={
 	origin:'*', 
