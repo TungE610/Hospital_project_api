31e9b87b7f836d62cb01fbb3ac8b7aa3684b7747
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2022-09-14 17:04:56 +0700

    fix server

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const cors = require('cors')
 const pool = require('../src/app/config/db.config')
 require('dotenv').config()
 const bcrypt = require('bcrypt')
-const port = process.env.PORT || 5000
+const port = process.env.DB_PORT || 5000
 app.listen(port, () => {
-	console.log(`The server is running in ${process.env.DB_PORT}`)
+	console.log(`The server is running in ${port}`)
 })
 const corsOptions ={
 	origin:'*', 
