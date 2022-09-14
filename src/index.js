05696642fe6e7aa7b42db44aca7537600dc25b30
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2022-09-15 04:19:48 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -256,7 +256,9 @@ app.get('/api/appointments/:appointment_id', async (req, res) => {
 	try {
 		const { appointment_id } = req.params
 		const appointments = await pool.query('SELECT appointment.appointment_id,appointment.start_time, diagnosis ,specialty.specialty,room_id,patient_id, doctor_id FROM appointment,specialty WHERE appointment.specialty_id = specialty.specialty_id AND appointment.appointment_id = $1',[appointment_id])
+		console.log("reuslt: ",appointments.rows[0] )
 		res.json(appointments.rows[0])
+
 	}catch(error){
 		console.log(error.message)
 	}
