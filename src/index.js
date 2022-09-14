27e800ad354844e33c9037064c2ff29228d3ef88
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2022-09-14 19:27:32 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
@@ -6,9 +7,9 @@ const cors = require('cors')
 const pool = require('../src/app/config/db.config')
 require('dotenv').config()
 const bcrypt = require('bcrypt')
-const port = process.env.DB_PORT || 5000
-app.listen(port, () => {
-	console.log(`The server is running in ${port}`)
+console.log(process.env.DB_PORT)
+app.listen(process.env.DB_PORT || 5000, () => {
+	console.log(`The server is running in ${process.env.DB_PORT || 5000}`)
 })
 const corsOptions ={
 	origin:'*', 
